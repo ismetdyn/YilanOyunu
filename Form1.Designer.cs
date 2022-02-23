8ea7d986705472253ec5150d2b3503d9de668d2d
ismet <ismet@ABRAA5>
2022-02-23 17:22:14 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Proje5._38_YilanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlAlan = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lYutulanYemSayisi = new System.Windows.Forms.Label();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlAlan
+            // 
+            this.pnlAlan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlAlan.Location = new System.Drawing.Point(12, 12);
+            this.pnlAlan.Name = "pnlAlan";
+            this.pnlAlan.Size = new System.Drawing.Size(562, 562);
+            this.pnlAlan.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 601);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yutulan Yem Sayısı:";
+            // 
+            // lYutulanYemSayisi
+            // 
+            this.lYutulanYemSayisi.AutoSize = true;
+            this.lYutulanYemSayisi.Location = new System.Drawing.Point(144, 601);
+            this.lYutulanYemSayisi.Name = "lYutulanYemSayisi";
+            this.lYutulanYemSayisi.Size = new System.Drawing.Size(14, 16);
+            this.lYutulanYemSayisi.TabIndex = 2;
+            this.lYutulanYemSayisi.Text = "0";
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(467, 593);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(107, 35);
+            this.btnBaslat.TabIndex = 3;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 639);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.lYutulanYemSayisi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlAlan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlAlan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lYutulanYemSayisi;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje5._38_YilanOyunu
+{
+    public partial class Form1 : Form
+    {
+
+        const Keys tusYukari = Keys.Up, tusSag = Keys.Right, tusAsagi = Keys.Down, tusSol = Keys.Left;
+        int XNoktasi, YNoktasi, XMaxBirim = 50, XMinBirim = 0, YMaxBirim = 50, YMinBirim = 0;
+        bool tusaBasildiMi = false;
+        int birBirim = 11;
+        string yon; //Yönler: ↑←→↓
+        Point yem;
+        List<Point> yilan = new List<Point>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            //Yılanı başlangıç noktasında üç bölmeli oluşturma
+            yilan.Clear();
+            yilan.Add(new Point(12, 20));
+            yilan.Add(new Point(11, 20));
+            yilan.Add(new Point(10, 20));
+
+            //Değerleri sıfırlama
+            XNoktasi = 12;
+            YNoktasi = 20;
+
+            yon = "→";
+
+            yemOlusturma();
+            yilaniCiz();
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if(btnBaslat.Text == "Başlat")
+            {
+                btnBaslat.Text = "Durdur";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                btnBaslat.Text = "Başlat";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Yılanın pozisyonunu belirleme
+            if (yon == "↓")
+                YNoktasi++;
+            else if (yon == "↑")
+                YNoktasi--;
+            else if (yon == "←")
+                XNoktasi--;
+            else if (yon == "→")
+                XNoktasi++;
+
+            bool oyunDurumu = false;
+            if (XNoktasi > XMaxBirim || XNoktasi < XMinBirim || YNoktasi > YMaxBirim ||
+                YNoktasi < YMinBirim)
+                oyunDurumu = true;
+
+            if (yilan.Any(t => t.X == XNoktasi && t.Y == YNoktasi))
+                oyunDurumu = true;
+
+            if (oyunDurumu == true)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Oyun Bitti!");
+                return;
+            }
+
+            yilan.Insert(0, new Point(XNoktasi, YNoktasi));
+            yilan.RemoveAt(yilan.Count - 1);
+
+            if (yem.X == XNoktasi * birBirim && yem.Y == YNoktasi * birBirim)
+            {
+                //Yemi yutma
+                Point lastPoint = yilan[yilan.Count - 1];
+                yilan.Add(new Point(lastPoint.X, lastPoint.Y));
+                lYutulanYemSayisi.Text = (yilan.Count - 3).ToString();
+                yemOlusturma();
+            }
+
+            yilaniCiz();
+            tusaBasildiMi = false;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys basilanTus)
+        {
+            if (timer1.Enabled && tusaBasildiMi == false)
+            {
+                tusaBasildiMi = true;
+                //Yön belirleme
+                switch (basilanTus)
+                {
+                    case tusYukari:
+                        if (yon != "↓")
+                            yon = "↑";
+                        break;
+                    case tusAsagi:
+                        if (yon != "↑")
+                            yon = "↓";
+                        break;
+                    case tusSol:
+                        if (yon != "→")
+                            yon = "←";
+                        break;
+                    case tusSag:
+                    default:
+                        if (yon != "←")
+                            yon = "→";
+                        break;
+                }
+            }
+            return base.ProcessCmdKey(ref msg, basilanTus);
+        }
+
+        private void yemOlusturma()
+        {
+            Random rastgele = new Random(DateTime.Now.TimeOfDay.Milliseconds);
+            int x = 0, y = 0;
+            bool bosYerVarMi = true;
+
+            while (bosYerVarMi)
+            {
+                x = rastgele.Next(XMinBirim, XMaxBirim + 1) * birBirim;
+                y = rastgele.Next(YMinBirim, YMaxBirim + 1) * birBirim;
+
+                bosYerVarMi = yilan.Any(t => t.X == x && t.Y == y);
+            }
+
+            yem = new Point(x, y);
+        }
+
+        private void yilaniCiz()
+        {
+            pnlAlan.Refresh();
+            Graphics g = this.pnlAlan.CreateGraphics();
+            Pen kalem = new Pen(Color.Blue, 5);
+            g.DrawRectangle(kalem, yem.X, yem.Y, 5, 5);
+            foreach (Point item in yilan)
+            {
+                int x = item.X * birBirim;
+                int y = item.Y * birBirim;
+
+                g.DrawRectangle(kalem, x, y, 5, 5);
+            }
+            kalem.Dispose();
+        }
+    }
+}
